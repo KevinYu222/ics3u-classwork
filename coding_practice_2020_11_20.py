ca9c92113363622fc4dc6cc8be179d29c7f270ad
KevY04 <60621073+KevinYu222@users.noreply.github.com>
2020-11-20 13:27:49 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -4,3 +4,14 @@ print(f"Wow! {color}?! That is also my favourite color!")
 can = int(input("How many cans are in a pack? "))
 pack = int(input("How many packs are there? "))
 print(f"You have total of {can * pack} cans.")
+
+length = float(input("Length: "))
+width = float(input("Width: "))
+height = float(input("Height: "))
+print(f"Volume: {length * width * height}")
+
+user = input("Did you just join a Google Meet and mute the teacher?")
+if user == "Yes" or user == "yes":
+    print("That's probably not a good idea.")
+elif user == "No" or user == "no":
+    print("Ok. Good.")
