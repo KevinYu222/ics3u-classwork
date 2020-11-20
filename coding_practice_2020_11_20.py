214d65f440e9dbd9498218d5ae7c554ad4b0e2d3
KevY04 <60621073+KevinYu222@users.noreply.github.com>
2020-11-20 13:29:34 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -10,7 +10,7 @@ width = float(input("Width: "))
 height = float(input("Height: "))
 print(f"Volume: {length * width * height}")
 
-user = input("Did you just join a Google Meet and mute the teacher?")
+user = input("Did you just join a Google Meet and mute the teacher? ")
 if user == "Yes" or user == "yes":
     print("That's probably not a good idea.")
 elif user == "No" or user == "no":
