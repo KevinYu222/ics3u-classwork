9f123ecfe14b6569b5b868e0828b0dcd8affe2ae
KevY04 <60621073+KevinYu222@users.noreply.github.com>
2020-11-20 13:40:50 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,9 +1,9 @@
 color = input("What is your favourite color? ")
 print(f"Wow! {color}?! That is also my favourite color!")
 
-can_per_pack = int(input("How many cans are in a pack? "))
+cans_per_pack = int(input("How many cans are in a pack? "))
 pack = int(input("How many packs are there? "))
-print(f"You have total of {can * pack} cans.")
+print(f"You have total of {cans_per_pack * pack} cans.")
 
 length = float(input("Length: "))
 width = float(input("Width: "))
