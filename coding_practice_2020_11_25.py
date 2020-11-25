12e1d5508873746d094428340d171b14ec00fc42
KevY04 <60621073+KevinYu222@users.noreply.github.com>
2020-11-25 14:00:55 -0500

    Create coding_practice_2020_11_25.py

---
+++ coding_practice_2020_11_25.py
@@ -0,0 +1,35 @@
+# Exercise 35
+while True:
+    try:
+        human_years = int(input("Enter human years: "))
+        if human_years < 0:
+            print("Error")
+        elif human_years <= 2:
+            print(f"{human_years * 10.5} dog years old.")
+            break
+        else:
+            print(f"{21 + ((human_years - 2)* 4)} dog years old.")
+            break
+    except ValueError:
+        print("You're suppose to enter a number.")
+
+# Exercise 44
+print("Check for fixed Holiday")
+valid_month = "january feburary march april".split(" ")
+print(valid_month)
+while True:
+    month = input("Month: ").lower
+    day = int(input("Day: "))
+
+    if month == "January" and day == 1:
+        print("It's New Year's Day")
+        break
+    elif month == "July" and day == 1:
+        print("It's Canada's Day")
+        break
+    elif month == "December" and day == 25:
+        print("It's Christmas Day")
+        break
+    else:
+        print("There is no fixed holiday that day.")
+        
