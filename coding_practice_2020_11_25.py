a09c7da276a2c7286b1f232594c7a0d83a0f46eb
KevY04 <60621073+KevinYu222@users.noreply.github.com>
2020-11-25 14:09:35 -0500

    Update coding_practice_2020_11_25.py

--- coding_practice_2020_11_25.py
+++ coding_practice_2020_11_25.py
@@ -1,3 +1,53 @@
+def make_out_word(out, word):
+  outside_1 = out[0:2]
+  outside_2 = out[2:4]
+  make_out_word = outside_1 + word + outside_2
+  return make_out_word
+
+def extra_end(str):
+  new_str = str[-2:len(str)]
+  extra_end = new_str + new_str + new_str
+  return extra_end
+
+def first_two(str):
+  first_two = str[0:2]
+  return first_two
+
+def first_half(str):
+  first_half = str[0:(len(str)/2)]
+  return first_half
+
+def without_end(str):
+  without_end = str[1:-1]
+  return without_end
+
+def left2(str):
+  left = str[2:len(str)]
+  right = str[0:2]
+  left2 = left + right
+  return left2
+
+def make_tags(tag, word):
+  make_tags = "<" + tag + ">" + word + "</" + tag + ">"
+  return make_tags
+
+def combo_string(a, b):
+  if len(a) > len(b):
+    combo_string = b + a + b
+  else:
+    combo_string = a + b + a
+  return combo_string
+
+def make_abba(a, b):
+  make_abba = a + b + b + a
+  return make_abba
+
+def non_start(a, b):
+  a = a[1:len(a)]
+  b = b[1:len(b)]
+  non_start = a + b
+  return non_start
+
 # Exercise 35
 while True:
     try:
