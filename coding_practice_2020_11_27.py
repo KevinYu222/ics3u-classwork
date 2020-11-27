a37fe649d672c8ea4d8255da64eeaa4bdb5aba2c
KevY04 <60621073+KevinYu222@users.noreply.github.com>
2020-11-27 13:59:02 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,50 @@
+#while loop exercise:
+a = 238
+while a <= 644:
+    print(a)
+    a += 14
+
+a = -225
+while a <= -17:
+    print(a)
+    a += 16
+
+a = -129
+while a <= 388:
+    print(a)
+    a += 11
+
+a = -445
+while a <= -211:
+    print(a)
+    a += 6
+
+a = 427
+while a <= 1021:
+    print(a)
+    a += 18
+
+a = 30
+while a <= 459:
+    print(a)
+    a += 13
+
+a = 159
+while a <= 313:
+    print(a)
+    a += 7
+
+a = -290
+while a <= 261:
+    print(a)
+    a += 19
+
+a = 334
+while a <= 677:
+    print(a)
+    a += 7
+
+a = -426
+while a <= -306:
+    print(a)
+    a += 6
