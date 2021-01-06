354b2ef8403d9cf26548be2492074d3e5d945a02
KevY04 <60621073+KevinYu222@users.noreply.github.com>
2021-01-06 09:02:34 -0500

    Update TPW_87.py

--- TPW_87.py
+++ TPW_87.py
@@ -1,1 +1,25 @@
+def main():
+    string = input("Please Enter a String: ")
+    try:
+        width = int(input(" Please Enter Length of the Width: "))
+    except ValueError:
+        print("Invalid, Please Enter a number.")
+    center_string(string, width)
 
+def center_string(string: str, width: int):
+    spaces = round((width - len(string))/2)
+    output = ""
+    for i in range(spaces):
+        output += " "
+    return output + string
+
+def tests():
+    assert center_string("a", 3) == " a"
+    assert center_string("aa", 4) == " aa"
+    assert center_string("b", 5) == "  b"
+    assert center_string("hello", 11) == "   hello"
+    print("all passed!")
+
+
+if __name__ == "__main__":
+    tests()
